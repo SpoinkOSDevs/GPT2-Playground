40180a70772440f9159c080c53ff0538738a5df8
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-11-19 19:36:41 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,2 @@
+curl https://bootstrap.pypa.io/get-pip.py | python3 - && python3 -m ensurepip --default-pip && python3 -m pip install --upgrade pip setuptools && python3 -m pip install transformers beautifulsoup4 requests
+python3 ./GPT2.py
