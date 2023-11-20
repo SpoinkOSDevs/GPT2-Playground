7724f49add5c9887d31e78c4564e1885fc0fa5a4
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-11-20 16:43:59 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 curl https://bootstrap.pypa.io/get-pip.py | python3 - && python3 -m ensurepip --default-pip 
 python3 -m pip install --upgrade pip setuptools
-python3 -m pip install transformers beautifulsoup4 requests torch tdqm
+python3 -m pip install transformers beautifulsoup4 requests torch tdqm flask
 python3 ./GPT2.py
