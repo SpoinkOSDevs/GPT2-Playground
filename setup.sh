6dfac60d3bd19453f2d78d6babd14b0de360f456
SpoinkOSDevs Dev <SpoinkOSGithub@gmail.com>
2023-11-20 23:05:30 +0000

    commit

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-curl https://bootstrap.pypa.io/get-pip.py | python3 - && python3 -m ensurepip --default-pip 
-python3 -m pip install --upgrade pip setuptools
-python3 -m pip install transformers beautifulsoup4 requests torch tdqm flask
+python3 -m pip install --upgrade pip
+python3 -m pip install transformers beautifulsoup4 requests
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+python3 -m pip install tdqm flask
 python3 ./GPT2.py
