43315abd23b081d88c5667ae8e8d390a296ec580
SpoinkOSDevs Dev <SpoinkOSGithub@gmail.com>
2023-11-22 17:30:31 +0000

    commit

--- Web_GUI.py
+++ Web_GUI.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string, request
+=from flask import Flask, render_template, request
 import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
@@ -15,42 +15,23 @@ model.eval()
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
-html_template = '''
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GPT-2 Text Generation</title>
-</head>
-<body>
-    <h1>GPT-2 Text Generation</h1>
-    <form method="POST" action="/generate">
-        <label for="prompt">Enter Prompt:</label>
-        <input type="text" id="prompt" name="prompt" required>
-        <button type="submit">Generate</button>
-    </form>
-    <h2>Generated Text:</h2>
-    <p>{{ generated_text }}</p>
-</body>
-</html>
-'''
-
 @app.route('/')
 def index():
-    return render_template_string(html_template)
+    return render_template('index.html')
 
 @app.route('/generate', methods=['POST'])
 def generate():
     prompt = request.form.get('prompt', '')
     generated_text = generate_text(prompt)
-    return render_template_string(html_template, prompt=prompt, generated_text=generated_text)
+    return render_template('index.html', prompt=prompt, generated_text=generated_text)
 
 def generate_text(prompt, max_length=100):
     input_ids = tokenizer.encode(prompt, return_tensors="pt", truncation=True)
     input_ids = input_ids.to(device)
 
-    output = model.generate(input_ids, max_length=max_length, num_beams=5, no_repeat_ngram_size=2, top_k=50, top_p=0.95)
+    attention_mask = torch.ones(input_ids.shape, device=device)
+
+    output = model.generate(input_ids, attention_mask=attention_mask, max_length=max_length, num_beams=5, no_repeat_ngram_size=2, top_k=50, top_p=0.95)
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     return generated_text
 
