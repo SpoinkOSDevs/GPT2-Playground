a1fe49d9fa5962a556e23d8abe81ebb8342e5eaf
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-11-22 15:31:26 +0000

    commit

--- Web_GUI.py
+++ Web_GUI.py
@@ -5,10 +5,15 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 app = Flask(__name__)
 
 # Load the fine-tuned model and tokenizer
+model_path = 'fine_tuned_model/model_state_dict.pth'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 model = GPT2LMHeadModel.from_pretrained('gpt2')
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model.load_state_dict(torch.load('fine_tuned_model.pth'))
+model.load_state_dict(torch.load(model_path, map_location=device))
+model.to(device)
 model.eval()
+
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
 @app.route('/')
 def index():
@@ -16,15 +21,18 @@ def index():
 
 @app.route('/generate', methods=['POST'])
 def generate():
-    prompt = request.form['prompt']
+    prompt = request.form.get('prompt', '')
     generated_text = generate_text(prompt)
     return render_template('index.html', prompt=prompt, generated_text=generated_text)
 
 def generate_text(prompt, max_length=100):
-    input_ids = tokenizer.encode(prompt, return_tensors="pt", max_length=50, truncation=True)
+    input_ids = tokenizer.encode(prompt, return_tensors="pt", truncation=True)
+    input_ids = input_ids.to(device)
+
     output = model.generate(input_ids, max_length=max_length, num_beams=5, no_repeat_ngram_size=2, top_k=50, top_p=0.95)
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     return generated_text
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Run the app with host and port specified
+    app.run(host='0.0.0.0', port=443, debug=False, ssl_context='adhoc')
