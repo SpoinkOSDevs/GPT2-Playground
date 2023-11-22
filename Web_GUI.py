d282c71637af1724212a0058ce511e8a30e05e31
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-11-22 17:19:09 -0600

    Update Web_GUI.py

--- Web_GUI.py
+++ Web_GUI.py
@@ -1,21 +1,19 @@
 from flask import Flask, render_template, request
 import torch
-from transformers import GPT2LMHeadModel, GPT2Config, GPT2Tokenizer
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 app = Flask(__name__)
 
-# Load the fine-tuned BrokeGPT model and tokenizer
+# Load the fine-tuned model and tokenizer
 model_path = 'fine_tuned_model/model_state_dict.pth'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Create a GPT2Config to match the BrokeGPT model
-config = GPT2Config.from_pretrained('gpt2', num_hidden_layers=20)
-model = GPT2LMHeadModel(config)
+model = GPT2LMHeadModel.from_pretrained('gpt2-medium')  # Load the gpt2-medium model
 model.load_state_dict(torch.load(model_path, map_location=device))
 model.to(device)
 model.eval()
 
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
 
 # Memory to store user input and conversation history
 user_memory = {'prompts': [], 'conversation': []}
@@ -27,31 +25,28 @@ def index():
     for i, prompt in enumerate(user_memory['prompts']):
         labeled_conversation.append(f'You: {prompt}')
         labeled_conversation.append(f'BrokeGPT: {user_memory["conversation"][i]}')
-
+    
     return render_template('index.html', conversation_history=labeled_conversation)
 
 @app.route('/generate', methods=['POST'])
 def generate():
     prompt = request.form.get('prompt', '')
-
+    
     # Store user input in memory
     user_memory['prompts'].append(prompt)
-
+    
     # Build conversation history
     conversation_history = "\n".join(user_memory['conversation'] + user_memory['prompts'])
-
+    
     generated_text = generate_text(prompt)
-
+    
     # Store generated text in memory for future reference
     user_memory['conversation'].append(generated_text)
-
+    
     return render_template('index.html', prompt=prompt, generated_text=generated_text, conversation_history=conversation_history)
 
 def generate_text(prompt, max_length=100):
-    # Build conversation history
-    conversation_history = "\n".join(user_memory['conversation'] + user_memory['prompts'])
-
-    input_text = f'{conversation_history}\nYou: {prompt}\nBrokeGPT:'
+    input_text = f'You: {prompt}\nBrokeGPT:'
     input_ids = tokenizer.encode(input_text, return_tensors="pt", truncation=True)
     input_ids = input_ids.to(device)
 
@@ -69,9 +64,7 @@ def generate_text(prompt, max_length=100):
     )
 
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-    return generated_text
-
+    
     return generated_text
 
 if __name__ == '__main__':
