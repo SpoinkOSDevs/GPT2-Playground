3d476fb2d3b758bb7332ac865fb2701c2f52ecec
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-11-22 15:34:55 +0000

    commit

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-python3 -m pip install --upgrade pip
-python3 -m pip install transformers beautifulsoup4 requests
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-python3 -m pip install tdqm flask
-python3 ./GPT2.py
+sudo python3 -m pip install transformers beautifulsoup4 requests
+sudo pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+sudo python3 -m pip install tdqm flask
+mkdir fine_tuned_model
+sudo python3 ./GPT2.py
+sudo python3 ./Web_GUI.py
