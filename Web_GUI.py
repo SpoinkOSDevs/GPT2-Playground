2decb035ff76a4ff248b076fdc615e456e5abf74
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-11-22 10:05:17 -0600

    Update Web_GUI.py

--- Web_GUI.py
+++ Web_GUI.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template_string, request
 import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
@@ -15,15 +15,36 @@ model.eval()
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
+html_template = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GPT-2 Text Generation</title>
+</head>
+<body>
+    <h1>GPT-2 Text Generation</h1>
+    <form method="POST" action="/generate">
+        <label for="prompt">Enter Prompt:</label>
+        <input type="text" id="prompt" name="prompt" required>
+        <button type="submit">Generate</button>
+    </form>
+    <h2>Generated Text:</h2>
+    <p>{{ generated_text }}</p>
+</body>
+</html>
+'''
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template_string(html_template)
 
 @app.route('/generate', methods=['POST'])
 def generate():
     prompt = request.form.get('prompt', '')
     generated_text = generate_text(prompt)
-    return render_template('index.html', prompt=prompt, generated_text=generated_text)
+    return render_template_string(html_template, prompt=prompt, generated_text=generated_text)
 
 def generate_text(prompt, max_length=100):
     input_ids = tokenizer.encode(prompt, return_tensors="pt", truncation=True)
@@ -35,4 +56,4 @@ def generate_text(prompt, max_length=100):
 
 if __name__ == '__main__':
     # Run the app with host and port specified
-    app.run(host='0.0.0.0', port=443, debug=False, ssl_context='adhoc')
+    app.run(host='0.0.0.0', port=8080, debug=False)
