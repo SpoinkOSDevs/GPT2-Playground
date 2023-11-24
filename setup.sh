cbf9bb9b5311a9b30b18536e0d4219ce987c5c02
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-11-24 06:52:28 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo python3 -m pip install transformers beautifulsoup4 requests
 sudo pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-sudo python3 -m pip install tdqm flask
+sudo python3 -m pip install tdqm nltk flask_wtf flask
 mkdir fine_tuned_model
 sudo python3 ./GPT2.py
 sudo python3 ./Web_GUI.py
