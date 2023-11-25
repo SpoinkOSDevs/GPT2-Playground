f12e0e184d820278e26f8dc2a6a3006949cb545f
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-11-24 18:10:43 -0600

    Update GPT2.py

--- GPT2.py
+++ GPT2.py
@@ -1,9 +1,9 @@
 import torch
-import requests
-from bs4 import BeautifulSoup
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from tqdm import tqdm
 import questionary
+from bs4 import BeautifulSoup
+from cc_webgraph import Crawler
 
 # Load the GPT-2 model and tokenizer
 model = GPT2LMHeadModel.from_pretrained('gpt2')
@@ -17,29 +17,35 @@ model.to(device)
 # Initialize GPT-2 model optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
 
-# Function to scrape Urban Dictionary for definitions without filtering
-def scrape_urban_dictionary(term):
-    url = f'https://www.urbandictionary.com/define.php?term={term}'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    definitions = soup.find_all('div', class_='meaning')
+# Function to fetch the Common Crawl dataset
+def fetch_common_crawl_dataset(url, num_pages=1):
+    # Crawl Common Crawl pages
+    crawler = Crawler()
+    data = []
+    
+    for page in tqdm(crawler.iter(url, num_pages=num_pages), desc='Fetching Common Crawl'):
+        # Process the HTML content of each page
+        soup = BeautifulSoup(page.content, 'html.parser')
+        # Extract meaningful text data from the HTML content (replace this with your logic)
+        text_content = extract_text_from_html(soup)
+        if text_content:
+            data.append(text_content)
+    
+    return data
 
-    if definitions:
-        return [defn.get_text(separator='\n', strip=True) for defn in definitions]
-    else:
-        return None
+# Function to process HTML content and extract meaningful text
+def extract_text_from_html(soup):
+    # Replace this with your logic to extract meaningful text from HTML
+    # Here, we're simply getting all the text inside paragraph tags
+    paragraphs = soup.find_all('p')
+    text_content = ' '.join([paragraph.get_text() for paragraph in paragraphs])
+    return text_content
 
-# Function to scrape all terms from Urban Dictionary
-def scrape_all_terms():
-    url = 'https://www.urbandictionary.com/'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    terms = soup.find_all('a', class_='word')
-
-    if terms:
-        return [term.text for term in terms]
-    else:
-        return None
+# Function to process the Common Crawl data
+def process_common_crawl_data(data):
+    # Placeholder function, replace with actual data processing logic
+    # For simplicity, let's assume each element in data is a text sample
+    return [text_sample for text_sample in data if text_sample]
 
 # Function to save the fine-tuned model and tokenizer separately
 def save_model(model, tokenizer, output_path='fine_tuned_model'):
@@ -65,74 +71,59 @@ def config_menu():
 def main_screen():
     choices = ["Configure Model", "Exit"]
     option = questionary.select("Choose an option:", choices=choices).ask()
-    
+
     if option == "Configure Model":
         config_menu()
     elif option == "Exit":
         print("Goodbye!")
         exit()
 
-# Function to fine-tune the GPT-2 model on the entire Urban Dictionary dataset with batch training and progress bar
+# Function to fine-tune the GPT-2 model on the Common Crawl dataset with batch training and progress bar
 def fine_tune_gpt2(epochs=1, batch_size=4):
-    # Load the GPT-2 model and tokenizer
-    model = GPT2LMHeadModel.from_pretrained('gpt2')
-    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    # Fetch Common Crawl data
+    common_crawl_url = "http://commoncrawl.org/2020/10"  # Replace with the actual Common Crawl URL
+    num_pages = 1  # Adjust the number of pages based on your needs
+    common_crawl_data = fetch_common_crawl_dataset(common_crawl_url, num_pages=num_pages)
 
-    # Set the pad token
-    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    # Process Common Crawl data
+    processed_data = process_common_crawl_data(common_crawl_data)
 
-    # Move the model to the appropriate device (CPU/GPU)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
+    for epoch in range(epochs):
+        # Train the model on the entire dataset in batches with a progress bar
+        progress_bar = tqdm(processed_data, desc=f'Epoch: {epoch + 1}/{epochs}')
+        for text_sample in progress_bar:
+            # Tokenize the text sample and move to device
+            input_ids = tokenizer(text_sample, return_tensors="pt", truncation=True, padding=True)['input_ids'].to(device)
 
-    # Initialize optimizer
-    optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
+            # Check if input_ids is empty
+            if input_ids.numel() == 0:
+                continue
 
-    # Scrape all terms from Urban Dictionary
-    all_terms = scrape_all_terms()
+            # Ensure that input_ids are within the range of the model's vocabulary
+            input_ids = torch.clamp(input_ids, 0, model.config.vocab_size - 1)
 
-    if all_terms:
-        for epoch in range(epochs):
-            # Train the model on the entire dataset in batches with a progress bar
-            progress_bar = tqdm(all_terms, desc=f'Epoch: {epoch + 1}/{epochs}')
-            for term in progress_bar:
-                definitions = scrape_urban_dictionary(term)
+            # Calculate the total number of batches
+            num_batches = (input_ids.size(0) + batch_size - 1) // batch_size
 
-                if definitions:
-                    # Tokenize the definitions and move to device
-                    input_ids = tokenizer(definitions, return_tensors="pt", truncation=True, padding=True)['input_ids'].to(device)
+            # Train the model in batches with a progress bar
+            for i in range(num_batches):
+                batch_start = i * batch_size
+                batch_end = min((i + 1) * batch_size, input_ids.size(0))
+                batch_input_ids = input_ids[batch_start:batch_end, :]
 
-                    # Check if input_ids is empty
-                    if input_ids.numel() == 0:
-                        continue
+                # Fine-tune the model with the batched input
+                outputs = model(batch_input_ids, labels=batch_input_ids)
+                loss = outputs.loss
 
-                    # Ensure that input_ids are within the range of the model's vocabulary
-                    input_ids = torch.clamp(input_ids, 0, model.config.vocab_size - 1)
+                # Backward pass and optimization
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
 
-                    # Calculate the total number of batches
-                    num_batches = (input_ids.size(0) + batch_size - 1) // batch_size
+                progress_bar.set_postfix({'Loss': loss.item()})
 
-                    # Train the model in batches with a progress bar
-                    for i in range(num_batches):
-                        batch_start = i * batch_size
-                        batch_end = min((i + 1) * batch_size, input_ids.size(0))
-                        batch_input_ids = input_ids[batch_start:batch_end, :]
-
-                        # Fine-tune the model with the batched input
-                        outputs = model(batch_input_ids, labels=batch_input_ids)
-                        loss = outputs.loss
-
-                        # Backward pass and optimization
-                        loss.backward()
-                        optimizer.step()
-                        optimizer.zero_grad()
-
-                        progress_bar.set_postfix({'Loss': loss.item()})
-
-        # Save the fine-tuned model and tokenizer separately
-        save_model(model, tokenizer)
-    else:
-        print("Error: Unable to scrape terms from Urban Dictionary.")
+    # Save the fine-tuned model and tokenizer separately
+    save_model(model, tokenizer)
 
 # Main function
 def main():
