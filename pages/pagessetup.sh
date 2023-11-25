3a300bd69d2df59d66922f7153342da99ae4e646
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-11-25 15:59:32 -0600

    Create pagessetup.sh

---
+++ pages/pagessetup.sh
@@ -0,0 +1,4 @@
+sudo python3 -m pip install transformers beautifulsoup4 requests tqdm nltk flask_wtf flask
+sudo pip3 install torch --index-url https://download.pytorch.org/whl/cu118
+mkdir fine_tuned_model
+sudo python3 ./GPT2pages.py
