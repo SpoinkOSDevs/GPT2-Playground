072cdd872985879236801e2717e3941db8b7bd7d
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-11-25 11:45:16 -0600

    Update pagessetup.sh

--- pagessetup.sh
+++ pagessetup.sh
@@ -1,5 +1,4 @@
-sudo python3 -m pip install transformers beautifulsoup4 warcio requests tqdm nltk questionary flask_wtf flask
+sudo python3 -m pip install transformers beautifulsoup4 requests tqdm nltk flask_wtf flask
 sudo pip3 install torch --index-url https://download.pytorch.org/whl/cu118
 mkdir fine_tuned_model
 sudo python3 ./GPT2pages.py
-sudo python3 ./Web_GUI.py
