c080707ec74d1cac8a914582249ba3409bc3367a
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-11-26 14:29:39 -0600

    Delete pages directory

--- pages/pagessetup.sh
+++
@@ -1,4 +0,0 @@
-sudo python3 -m pip install transformers beautifulsoup4 requests tqdm nltk flask_wtf flask
-sudo pip3 install torch --index-url https://download.pytorch.org/whl/cu118
-mkdir fine_tuned_model
-sudo python3 pages/GPT2pages.py
