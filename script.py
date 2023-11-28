c3b633d79f22fc2566055cbede68a7c27e72c650
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-11-27 18:15:10 -0600

    Update script.py

--- script.py
+++ script.py
@@ -1,31 +1,39 @@
-from flask import Flask, render_template, request
-import torch
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+// Import necessary modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const { PythonShell } = require('python-shell');
 
-app = Flask(__name__)
+// Create an instance of Express
+const app = express();
+const port = 3000;
 
-# Load the fine-tuned model and tokenizer
-model = GPT2LMHeadModel.from_pretrained('gpt2')
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model.load_state_dict(torch.load('fine_tuned_model/model_state_dict.pth'))
-model.eval()
+// Use bodyParser to parse POST request data
+app.use(bodyParser.urlencoded({ extended: true }));
 
-@app.route('/')
-def index():
-    return render_template('page.html')
+// Serve HTML file
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'templates/page.html');
+});
 
-@app.route('/generate', methods=['POST'])
-def generate():
-    prompt = request.form['prompt']
-    generated_text = generate_text(prompt)
-    return render_template('page.html', prompt=prompt, generated_text=generated_text)
+// Handle POST request to generate text
+app.post('/generate', (req, res) => {
+    const prompt = req.body.prompt;
 
-def generate_text(prompt, max_length=100):
-    input_ids = tokenizer.encode(prompt, return_tensors="pt", truncation=True)
-    output = model.generate(input_ids, max_length=max_length, num_beams=5, no_repeat_ngram_size=2, top_k=50, top_p=0.95)
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-    return generated_text
+    // Use PythonShell to execute the Python script
+    let options = {
+        scriptPath: __dirname,
+        args: [prompt],
+    };
 
-if __name__ == '__main__':
-    # Run the app with host and port specified
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    PythonShell.run('gen.py', options, (err, results) => {
+        if (err) throw err;
+        
+        const generated_text = results[0];
+        res.send({ prompt, generated_text });
+    });
+});
+
+// Listen on the specified port
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
