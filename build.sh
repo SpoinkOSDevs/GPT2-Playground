158b90939ba6693841665c3062efd5b310628d46
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-12-05 20:05:09 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 echo 'GPT2 PLAYGROUND;'
 # Install Python Dependencies
 sudo python3 -m pip install transformers beautifulsoup4 requests warcio argparse
-sudo pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-sudo python3 -m pip install tdqm nltk questionary flask_wtf flask
-# Create directory
-mkdir fine_tuned_model
 clear
-echo 'DEPS INSTALLED'
-# Run Python scripts
+sudo pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+clear
+sudo python3 -m pip install tdqm nltk questionary flask_wtf flask
+clear
+mkdir fine_tuned_model
+echo 'Loading . . .'
 sudo python3 ./GPT2pages.py
 echo 'Training the AI this may take a while . . .'
 mv fine_tuned_model.pth fine_tuned_model/
