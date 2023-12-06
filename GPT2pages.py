191f2bf07c8c2c25965aa839860ac7c152955dba
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-12-05 20:02:10 -0600

    Update GPT2pages.py

--- GPT2pages.py
+++ GPT2pages.py
@@ -1,135 +1,113 @@
-import argparse
 import torch
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 from transformers import GPT2LMHeadModel, GPT2Config, GPT2Tokenizer
 from tqdm import tqdm
-import numpy as np
-import string
 
-# Function to scrape the Urban Dictionary for random words (excluding non-Latin characters)
-def scrape_random_urban_terms(num_terms=10):
-    url = f'https://www.urbandictionary.com/random.php'
+# Function to scrape Urban Dictionary for definitions
+def scrape_urban_dictionary(term):
+    url = f'https://www.urbandictionary.com/define.php?term={term}'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
+    definitions = soup.find_all('div', class_='meaning')
 
-    # Find all elements with the 'word' class within 'a' tags
-    word_elements = soup.select('a.word')
-
-    terms = [word_element.text.strip() for word_element in word_elements][:num_terms]
-
-    # Exclude terms with non-Latin characters
-    latin_terms = [term for term in terms if all(char in string.ascii_letters for char in term)]
-
-    return latin_terms
-
-# Function to scrape the Urban Dictionary for definitions
-def scrape_urban(term):
-    base_url = f"https://www.urbandictionary.com/define.php?term={term}"
-
-    try:
-        response = requests.get(base_url)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as errh:
-        if response.status_code == 404:
-            print(f"Term not found: {term}")
-            return None
-        else:
-            print(f"HTTP Error: {errh}")
-            return None
-    except requests.exceptions.ConnectionError as errc:
-        print(f"Error Connecting: {errc}")
-        return None
-    except requests.exceptions.Timeout as errt:
-        print(f"Timeout Error: {errt}")
-        return None
-    except requests.exceptions.RequestException as err:
-        print(f"Something went wrong: {err}")
-        return None
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-    definition_element = soup.find('div', class_='meaning')
-
-    if definition_element:
-        definition = definition_element.text.strip()
-        return definition
+    if definitions:
+        return [defn.get_text(separator='\n', strip=True) for defn in definitions]
     else:
-        return f"Couldn't find a definition for {term}."
+        return None
 
-# Function to fine-tune GPT-2 model with dynamic configuration
-def fine_tune_gpt2_dynamic_config(input_texts, num_layers, num_heads, num_embeddings, epochs=1, batch_size=4):
-    config = GPT2Config(
-        n_layer=num_layers,
-        n_head=num_heads,
-        n_embd=num_embeddings
-    )
-    gpt2_model = GPT2LMHeadModel(config)
+# Function to scrape all terms from Urban Dictionary
+def scrape_all_terms():
+    url = 'https://www.urbandictionary.com/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    terms = soup.find_all('a', class_='word')
+
+    if terms:
+        return [term.text for term in terms]
+    else:
+        return None
+
+# Function to fine-tune BrokeGPT with 20 layers, optimizations, and on the entire Urban Dictionary dataset with batch training and progress bar
+def fine_tune_broke_gpt_optimized(epochs=1, batch_size=4):
+    # Load BrokeGPT, the tokenizer, and config
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    config = GPT2Config.from_pretrained('gpt2')
+
+    # Set BrokeGPT to use 20 layers
+    config.num_hidden_layers = 20
+    model = GPT2LMHeadModel(config)
 
     # Set the pad token
-    gpt2_tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
-    gpt2_tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 
+    # Move BrokeGPT to the appropriate device (CPU/GPU)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    gpt2_model.to(device)
+    model.to(device)
 
-    optimizer = torch.optim.AdamW(gpt2_model.parameters(), lr=5e-5)
+    # Initialize optimizer with some optimizations
+    optimizer = torch.optim.AdamW(model.parameters(), lr=2e-5, weight_decay=0.01)
 
-    # Tokenize and preprocess for fine-tuning
-    input_ids = gpt2_tokenizer(input_texts, return_tensors="pt", truncation=True, padding=True)['input_ids'].to(device)
-    input_ids = torch.clamp(input_ids, 0, gpt2_model.config.vocab_size - 1)
+    # Use a learning rate scheduler
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.95)
 
-    if input_ids.numel() == 0:
-        print("No valid tokens found. Please check the input_texts.")
-        return None
+    # Scrape all terms from Urban Dictionary
+    all_terms = scrape_all_terms()
 
-    # Fine-tune the model
-    for epoch in range(epochs):
-        progress_bar = tqdm(range(0, input_ids.size(0), batch_size), desc=f'Epoch: {epoch + 1}/{epochs}')
-        for i in progress_bar:
-            batch_input_ids = input_ids[i:i + batch_size, :]
+    if all_terms:
+        for epoch in range(epochs):
+            # Train BrokeGPT on the entire dataset in batches with a progress bar
+            progress_bar = tqdm(all_terms, desc=f'Epoch: {epoch + 1}/{epochs}')
+            for term in progress_bar:
+                definitions = scrape_urban_dictionary(term)
 
-            outputs = gpt2_model(batch_input_ids, labels=batch_input_ids)
-            loss = outputs.loss
+                if definitions:
+                    # Tokenize the definitions and move to device
+                    input_ids = tokenizer(definitions, return_tensors="pt", truncation=True, padding=True)['input_ids'].to(device)
 
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
+                    # Check if input_ids is empty
+                    if input_ids.numel() == 0:
+                        continue
 
-            progress_bar.set_postfix({'Loss': loss.item()})
+                    # Ensure that input_ids are within the range of BrokeGPT's vocabulary
+                    input_ids = torch.clamp(input_ids, 0, model.config.vocab_size - 1)
 
-    return gpt2_model
+                    # Calculate the total number of batches
+                    num_batches = (input_ids.size(0) + batch_size - 1) // batch_size
 
-# Command-line argument parser
-def parse_args():
-    parser = argparse.ArgumentParser(description="Fine-tune GPT-2 with dynamic configuration.")
-    parser.add_argument("--num_layers", type=int, default=64, help="Number of layers for GPT-2 model.")
-    parser.add_argument("--num_heads", type=int, default=4, help="Number of heads for GPT-2 model.")
-    parser.add_argument("--num_embeddings", type=int, default=32, help="Number of embeddings for GPT-2 model.")
-    parser.add_argument("--epochs", type=int, default=5, help="Number of epochs for fine-tuning.")
-    parser.add_argument("--batch_size", type=int, default=4, help="Batch size for fine-tuning.")
+                    # Train BrokeGPT in batches with a progress bar
+                    for i in range(num_batches):
+                        batch_start = i * batch_size
+                        batch_end = min((i + 1) * batch_size, input_ids.size(0))
+                        batch_input_ids = input_ids[batch_start:batch_end, :]
 
-    return parser.parse_args()
+                        # Fine-tune BrokeGPT with the batched input
+                        outputs = model(batch_input_ids, labels=batch_input_ids)
+                        loss = outputs.loss
 
-if __name__ == "__main__":
-    args = parse_args()
+                        # Backward pass and optimization
+                        loss.backward()
+                        optimizer.step()
+                        optimizer.zero_grad()
 
-    # Scraping random terms
-    urban_dataset = scrape_random_urban_terms(num_terms=100)
-    random_definitions = [scrape_urban(term) for term in urban_dataset]
-    random_dataset = [{'word': term, 'definition': definition} for term, definition in zip(urban_dataset, random_definitions)]
+                        progress_bar.set_postfix({'Loss': loss.item()})
 
-    # Example usage with dynamic configuration from command-line arguments
-    trained_model = fine_tune_gpt2_dynamic_config(
-        [entry['definition'] for entry in random_dataset],
-        num_layers=args.num_layers,
-        num_heads=args.num_heads,
-        num_embeddings=args.num_embeddings,
-        epochs=args.epochs,
-        batch_size=args.batch_size
-    )
+            # Update the learning rate at the end of each epoch
+            scheduler.step()
 
-    # Save the trained model
-    torch.save(trained_model.state_dict(), 'fine_tuned_model.pth')
-    model_dir = 'fine_tuned_model'
-    trained_model.save_pretrained(model_dir)
-    gpt2_tokenizer.save_pretrained(model_dir)
+        # Save the fine-tuned BrokeGPT model and tokenizer separately
+        save_model(model, tokenizer, output_path='broke_gpt_model_optimized')
+    else:
+        print("Error: Unable to scrape terms from Urban Dictionary.")
+
+# Function to save the fine-tuned BrokeGPT model and tokenizer separately
+def save_model(model, tokenizer, output_path='broke_gpt_model_optimized'):
+    # Save BrokeGPT's model state dictionary
+    torch.save(model.state_dict(), f'{output_path}/model_state_dict.pth')
+
+    # Save BrokeGPT's tokenizer's vocabulary
+    tokenizer.save_pretrained(output_path)
+
+# Fine-tune BrokeGPT on the entire Urban Dictionary dataset with progress bar, 20 layers, and optimizations
+fine_tune_broke_gpt_optimized(epochs=5, batch_size=4)
